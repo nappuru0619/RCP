81361462e802b61a8f1d3aed5c73e6f9900965ac
nappuru <73878007+nappuru0619@users.noreply.github.com>
2021-02-04 22:06:24 +0900

    いろいろなところにミスがあったので訂正

--- src/main/java/rcp_plugin/rcp/RCP.java
+++ src/main/java/rcp_plugin/rcp/RCP.java
@@ -13,11 +13,12 @@ public final class RCP extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("RCPプラグインが起動しました");
+
         String[] hand = {"グー", "チョキ", "パー"};
         //じゃんけんで出す手の配列
         Random r = new Random();
         //乱数を生成するメソッド
-        enemy = hand[r.nextInt()];
+        enemy = hand[r.nextInt(3)];
         //相手の手をランダムで決める
     }
 
@@ -28,15 +29,15 @@ public final class RCP extends JavaPlugin {
 
             if (args.length == 0) return false;
 
-            if (args[0].equalsIgnoreCase("パー") && enemy.equalsIgnoreCase("チョキ") ||
-                    args[0].equalsIgnoreCase("チョキ") && enemy.equalsIgnoreCase("グー") ||
-                    args[0].equalsIgnoreCase("グー") && enemy.equalsIgnoreCase("パー")) {
+            if(args[0].equalsIgnoreCase("paa") && enemy.equalsIgnoreCase("tyo") ||
+                    args[0].equalsIgnoreCase("tyo") && enemy.equalsIgnoreCase("goo") ||
+                    args[0].equalsIgnoreCase("goo") && enemy.equalsIgnoreCase("paa")){
                 //敗北のパターン
-                sender.sendMessage("§l俺の勝ち! 何で負けたか明日まで考えといてください");
+                sender.sendMessage("俺の勝ち! 何で負けたか明日まで考えといてください");
 
-            } else if (args[0].equalsIgnoreCase("チョキ") && enemy.equalsIgnoreCase("パー") ||
-                    args[0].equalsIgnoreCase("グー") && enemy.equalsIgnoreCase("チョキ") ||
-                    args[0].equalsIgnoreCase("パー") && enemy.equalsIgnoreCase("グー")) {
+            }else if(args[0].equalsIgnoreCase("tyo") && enemy.equalsIgnoreCase("paa") ||
+                    args[0].equalsIgnoreCase("goo") && enemy.equalsIgnoreCase("tyo") ||
+                    args[0].equalsIgnoreCase("paa") && enemy.equalsIgnoreCase("goo")) {
                 //勝利のパターン
                 sender.sendMessage("§lやるやん! 明日は俺にリベンジさせて");
 
